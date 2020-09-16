bd89bfea8d4aca64999353d09c518c5f04ff81e9
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-16 00:21:47 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,4 +1,3 @@
-#This is the groceries hash we'll be passing in to the get_the_values method
 groceries = {
   dairy: ["milk", "yogurt", "cheese"],
   vegetable: ["carrots", "broccoli", "cucumbers"],
